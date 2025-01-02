629c32ba4964e99284794a5c42a49a1232ca6e37
mongmolong <143096636+mongmolong@users.noreply.github.com>
2025-01-02 21:15:32 +0900

    readme

--- ex.js
+++
@@ -1,8 +0,0 @@
-// function clickme() {
-//     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-// }
-
-
-// const video = document.querySelector('.mediaSub video');
-// video.addEventListener('suspend',()=>{});
-// video.addEventListener('play',()=>{});
